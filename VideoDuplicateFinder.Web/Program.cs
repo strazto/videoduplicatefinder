61a03cc564006bde8c847a1faf55ab8d10ee7401
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-07-11 16:31:32 +1000

    Configure logging using ILogger injection - Use serilog, and CLI args to configure logging server
    Squash of:
    * configure logging using ILogger interface
    * Use serilog logging provider
    * Accept hostbuilder args & logsink args
      Added runtime args:
      * --log-sink
      * --host-builder-args

--- VideoDuplicateFinder.Web/Program.cs
+++ VideoDuplicateFinder.Web/Program.cs
@@ -2,22 +2,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.CommandLine; 
+
 namespace DuplicateFinderWeb
 {
     public class Program
     {
-        public static void Main(string[] args)
+		/// <param name="logSink">The URL of the seq instance to use as a log sink</param>
+		/// <param name="hostBuilderArgs">Args to pass onto the host builder</param>
+        public static void Main(string logSink, string[] hostBuilderArgs)
         {
-            CreateHostBuilder(args).Build().Run();
+			Console.Error.WriteLine($"Logging to {logSink}");
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.WriteTo.Seq(logSink)
+				.WriteTo.Console()
+				.CreateLogger();
+
+            CreateHostBuilder(hostBuilderArgs).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureLogging(
+					logging => {
+						logging.AddSerilog();
+				})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();

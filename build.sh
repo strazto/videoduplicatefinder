5805a1a1c12db62c978ef777bc27ba0003328079
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-07-17 15:00:32 +1000

    Build: Write each project+platform combo to a separate folder once again (#14)
    ## Build: Write each project+platform combo to a separate folder once again
    
    Previously it seemed to work okay to write all binaries for a platform to same folder, but now this doesn't work.
    It's possible ASP.net runtime binaries aren't compatible with regular runtime binaries, I don't know.
    
    ## Build: Extract ffmpeg bundle to function, zip each projects
    
    Make ffmpeg bundling more consistent -
    Now searches for ffmpeg to bundle in Releases/ffmpeg/<platform-id>
    
    If found, copies that to VDF.project-platform-id/bin

--- build.sh
+++ build.sh
@@ -9,8 +9,32 @@ function get_out {
 	local TARGET_PROJECT="$2"
 	local BASE_DIR="$3"
 
-	local TARGET_OUT="${BASE_DIR}/VDF-${TARGET_PLATFORM}"
+	local TARGET_OUT="${TARGET_PROJECT/VideoDuplicateFinder/VDF}"
+	local TARGET_OUT="${BASE_DIR}/${TARGET_OUT}-${TARGET_PLATFORM}"
 	echo "$TARGET_OUT"
+}
+
+function bundle_ffmpeg {
+	local USAGE="Usage: bundle_ffmpeg <target_platform> <target_folder_out> <ffmeg_parent_dir>"
+	if [[ "$#" -lt "3" ]] || [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
+		echo "$USAGE"
+		exit 1
+	fi
+	local TARGET_PLATFORM="$1"
+	local TARGET_OUT="$2"
+	local FFMPEG_PARENT="$3"
+
+	echo "${TARGET_PLATFORM}.FFMPEG: ====================================================="
+
+	FFMPEG_DIR="$FFMPEG_PARENT/ffmpeg/${TARGET_PLATFORM}" 
+	if [[ -d "$FFMPEG_DIR" ]]; then
+		echo "${TARGET_PLATFORM}.FFMPEG: Found ${FFMPEG_DIR} copying => ${TARGET_OUT}/bin"
+
+		mkdir -p "${TARGET_OUT}/bin/"
+		cp ${FFMPEG_DIR}/*.* "${TARGET_OUT}/bin/"
+	else
+		echo "${TARGET_PLATFORM}.FFMPEG: No ${FFMPEG_DIR}"
+	fi
 }
 
 function publish {
@@ -29,31 +53,32 @@ function publish {
 		-r "$TARGET_PLATFORM" \
 		-o "$TARGET_OUT" \
 		"$TARGET_PROJECT"
+
+	bundle_ffmpeg "$TARGET_PLATFORM" "$TARGET_OUT" "$BASE_DIR"
 }
 
+# Publish for windows
 
 PLATFORM=win-x64
 PROJECTS=("VideoDuplicateFinder.Windows" "VideoDuplicateFinder.Web" "VideoDuplicateFinder.Console")
 BASE_DIR="Releases"
 
+rm -r $BASE_DIR/VDF*
+
 for project in ${PROJECTS[@]}; do
-	echo "Building $PLATFORM $project"
-	TARGET_OUT="$(get_out $TARGET_PLATFORM $TARGET_PROJECT $BASE_DIR)"
+	echo "========== ${project}-${PLATFORM} ============================"
 	publish "$PLATFORM" "$project" "$BASE_DIR"
-
-
 done
 
-if [[ -d "$BASE_DIR/ffmpeg" ]]; then
-	mkdir -p "${TARGET_OUT}/bin/"
-	cp ${BASE_DIR}/ffmpeg/x64/*.* "${TARGET_OUT}/bin/"
-	
-fi
+# Publish for linux
 
 PLATFORM=linux-x64
 PROJECTS=("VideoDuplicateFinderLinux" "VideoDuplicateFinder.Web" "VideoDuplicateFinder.Console")
 
 for project in ${PROJECTS[@]}; do
+	echo "========== ${project}-${PLATFORM} ============================"
 	publish "$PLATFORM" "$project" "$BASE_DIR"
 done
 
+# Zip each project
+find "$BASE_DIR" -maxdepth 1 -type d -name 'VDF*' -execdir '/bin/sh' '-c' 'echo "{} => {}.zip" ; zip -qr "{}.zip" "{}"' ';'
